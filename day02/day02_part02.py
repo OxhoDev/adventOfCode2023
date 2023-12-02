30269c98482ed58ce6d18ac8a9f50da56cc8e5fa
Régis Lespinasse <regis.lespinasse@mxns.com>
2023-12-02 18:04:59 +0100

    day02 yay

---
+++ day02/day02_part02.py
@@ -0,0 +1,39 @@
+import re
+
+#file_name = '/home/rle/Documents/adventOfCode2023/day02/example.txt'
+file_name = '/home/rle/Documents/adventOfCode2023/day02/data.txt'
+
+
+result = 0
+#line = 'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red'
+
+
+
+with open(file_name, 'r') as input_file:
+    for line in input_file:
+        game = line.rstrip('\n').split(':')
+        gameResult = game[1]
+        gameResult = gameResult.replace(';', ',')
+
+        redTable = []
+        greenTable = []
+        blueTable = []
+        for res in gameResult.split(','):
+            drawResult = res.strip().split(' ')
+            #print(drawResult)
+
+            match drawResult[1]:
+                case 'red':
+                    #print('red')
+                    redTable.append(int(drawResult[0]))
+                    #print(redTable)
+                case 'green':
+                    #print('green')
+                    greenTable.append(int(drawResult[0]))
+                case 'blue':
+                    #print('blue')
+                    blueTable.append(int(drawResult[0]))
+
+        result += max(redTable) * max(greenTable) * max(blueTable)
+
+print(result)
