055647e62e71651b70952731e8896ac74c8c41ab
Régis Lespinasse <regis.lespinasse@mxns.com>
2023-12-06 17:58:09 +0100

    yay (arch lol) part 01

--- day06/day06_part01.py
+++ day06/day06_part01.py
@@ -6,8 +6,37 @@ file_name = '/home/rle/Documents/adventOfCode2023/day06/files/example.txt'
 result = 0
 reDigits = "[^z0-9]"
 
-myLines = open(file_name, 'r').readlines()
+### Functions
 
+def count_max_value (distance):
+    print('WIP')
+
+
+### Main
+
+myLines = open(file_name, 'r').readlines()
 races = [s for s in re.split(reDigits, myLines[0]) if s != '']
 
 print(races)
+
+for r in (races):
+    distance = []
+    for t in range(0, int(r)+1):
+        print(t)
+        speed = t
+        d = r - t       
+        distance.append(d)
+    
+    #maxDistance = count_max_value(distance)
+
+
+#'''
+#Pour chaque course
+#    Pour chaque milisec
+#        on calcule la distance pour chaque input
+#        on stock dans un tableau []
+
+
+#        function: on compte le nombre de fois qu'on trouve le max[ditance]
+#            on renvoit
+#'''
