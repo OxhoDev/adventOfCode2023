85d0b666299c5204cc0e15db14826b4d86469ec3
Régis Lespinasse <regis.lespinasse@mxns.com>
2023-12-06 09:46:25 +0100

    day06 start

--- day06/day06_part01.py
+++ day06/day06_part01.py
@@ -1,6 +1,13 @@
 import re
-import os
+#import os
 
-#file_name = '/home/rle/Documents/adventOfCode2023/day05/files/example.txt'
-file_name = '/home/rle/Documents/adventOfCode2023/day05/files/data.txt'
+file_name = '/home/rle/Documents/adventOfCode2023/day06/files/example.txt'
+#file_name = '/home/rle/Documents/adventOfCode2023/day06/files/data.txt'
 result = 0
+reDigits = "[^z0-9]"
+
+myLines = open(file_name, 'r').readlines()
+
+races = [s for s in re.split(reDigits, myLines[0]) if s != '']
+
+print(races)
