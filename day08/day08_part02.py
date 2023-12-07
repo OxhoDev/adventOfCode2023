1427a0915f5dd3abf4929bb81423d522fb2b2485
Régis Lespinasse <regis.lespinasse@mxns.com>
2023-12-07 16:34:51 +0100

    prepare next days

---
+++ day08/day08_part02.py
@@ -0,0 +1,8 @@
+import re
+import os
+
+file_name = '/home/rle/Documents/adventOfCode2023/day08/files/example.txt'
+#file_name = '/home/rle/Documents/adventOfCode2023/day08/files/data.txt'
+result = 0
+
+### Func
---
+++ day09/day09_part01.py
@@ -0,0 +1,8 @@
+import re
+import os
+
+file_name = '/home/rle/Documents/adventOfCode2023/day09/files/example.txt'
+#file_name = '/home/rle/Documents/adventOfCode2023/day09/files/data.txt'
+result = 0
+
+### Func
